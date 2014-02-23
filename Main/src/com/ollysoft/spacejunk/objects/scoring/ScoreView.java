028630f5984895608c1d4b113f07417a646c4251
Olly Oechsle <owksley@gmail.com>
2014-02-23 07:52:11 +0000

    Animating score display

--- Main/src/com/ollysoft/spacejunk/objects/scoring/ScoreView.java
+++ Main/src/com/ollysoft/spacejunk/objects/scoring/ScoreView.java
@@ -8,17 +8,31 @@ import com.ollysoft.spacejunk.util.Assets;
 
 public class ScoreView extends Actor {
 
+  private static final float POINTS_PER_SECOND = 60;
   private final BitmapFont font;
   private final ScoreModel model;
+  private float displayScore;
 
   public ScoreView(Assets assets, ScoreModel model) {
     this.model = model;
     this.font = assets.bigFont;
+    this.displayScore = 0f;
+  }
+
+  @Override
+  public void act(float delta) {
+    super.act(delta);
+    int actualScore = model.getScore();
+    if (actualScore >= displayScore) {
+      displayScore += POINTS_PER_SECOND * delta;
+    } else {
+      displayScore = actualScore;
+    }
   }
 
   @Override
   public void draw(SpriteBatch batch, float parentAlpha) {
-    String fontText = "" + model.getScore();
+    String fontText = "" + (int) displayScore;
     BitmapFont.TextBounds bounds = font.getBounds(fontText);
     float fontX = GameScreen.width - font.getBounds(fontText).width;
     float fontY = GameScreen.height - bounds.height;
